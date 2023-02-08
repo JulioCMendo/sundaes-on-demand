406dd5d96168ce5401eb6c178b84a5858e56cc10
Júlio Cezar Mendo <mendo.julio.c@gmail.com>
2023-02-07 21:46:34 -0300

    Code and tests for Toppings

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -13,3 +13,21 @@ test('displays image for each scoop option from server', async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('displays images for each topping option from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  // find image
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Paçoquinha topping'
+  ]);
+});
